970e5df083bfb76ff7b5b5b0f2c7a92f6118a23e
Adinath0107 <166931227+Adinath0107@users.noreply.github.com>
2025-04-02 13:08:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+// type module
+import fs from "fs/promises"
+import fsn from "fs" //fs normal
+import path from "path"
+
+const basepath = "C:\\webdev\\javascript\\93 clear the clutter"
+let files =  await fs.readdir(basepath)
+//console.log(files);
+
+for (const item of files) {
+  //   let extension = item.split(".")  //array eg output [ 'FunctionOverloading1', 'cpp' ]
+  // console.log(extension);
+  //last element of array is the extension
+
+  let extension = item.split(".")[item.split(".").length -1]
+ // console.log(extension); 
+ /* jpg
+    pdf
+    cpp
+    svg  ... */
+
+    if(  (extension != "js" && extension != "json") && item.split(".").length > 1)  //item.split(".").length > 1 it is not a folder
+    {
+
+            if(fsn.existsSync(path.join(basepath, extension)))//further path when folder created
+            { 
+                // Move the file to this directory if its not a js or json file
+                //.rename to move the file into folder
+                fs.rename(path.join(basepath, item), path.join(basepath, extension, item))
+        
+            }
+            else
+            {
+                fs.mkdir(extension); //make folder for each extension
+                fs.rename(path.join(basepath, item), path.join(basepath, extension, item)) //add first file of that extension
+            }
+    }
+       
+
+  
+   
+    
+}
